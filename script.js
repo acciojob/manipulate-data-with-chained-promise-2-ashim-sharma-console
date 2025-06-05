535f8718cf38ea585b753f543ac70ab5df43f502
ashim-sharma-console <ashimsharma49@gmail.com>
2025-06-06 01:27:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
-//your JS code here. If required.
+
+ const outputDiv = document.getElementById('output');
+    const arr = [1, 2, 3, 4];
+
+  
+    function getInitialArray() {
+      return new Promise(function(resolve) {
+        setTimeout(function() {
+          resolve(arr);
+        }, 3000);
+      });
+    }
+
+   
+    function filterEvenNumbers(array) {
+      return new Promise(function(resolve) {
+        setTimeout(function() {
+          const evenNumbers = array.filter(function(num) {
+            return num % 2 === 0;
+          });
+        
+          outputDiv.textContent = evenNumbers.join(',');
+          resolve(evenNumbers);
+        }, 1000);
+      });
+    }
+
+  
+    function multiplyByTwo(array) {
+      return new Promise(function(resolve) {
+        setTimeout(function() {
+          const multiplied = array.map(function(num) {
+            return num * 2;
+          });
+         
+          outputDiv.textContent = multiplied.join(',');
+          resolve(multiplied);
+        }, 2000);
+      });
+    }
+
+   
+    getInitialArray()
+      .then(filterEvenNumbers)
+      .then(multiplyByTwo);
